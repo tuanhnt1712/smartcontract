84d41616f92661b22684c11985d742d95cc65cfc
tuanhnt1712 <tuanhnt1712@gmail.com>
2018-08-21 14:38:03 +0700

    first commit

--- cmd/main.go
+++ cmd/main.go
@@ -22,4 +22,14 @@ func main() {
 	ethclient := ethclient.NewClient(client)
 	bc := blockchain.NewBlockchain(client, ethclient, config.Signer, config.Contract, config.AbiPath)
 	bc.GetTreasure()
+	owner := "0x2cc72d8857ac57ba058eddd36b2f14adc2a058bd"
+	var amount int64 = 100
+
+	amountBig := big.NewInt(amount)
+
+	tx, err := bc.AddTreasure(owner, amountBig)
+	if err != nil {
+		log.Println(err)
+	}
+	log.Println("raw tx: ", tx, tx.Hash().String())
 }
